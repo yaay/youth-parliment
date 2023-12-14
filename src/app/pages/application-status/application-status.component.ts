a6d53ecbc40d0c56761c37e196771ecf23575015
rawan osama <rwanosama724@gmail.com>
2023-12-14 11:56:25 +0200

    handle request

--- src/app/pages/application-status/application-status.component.ts
+++ src/app/pages/application-status/application-status.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { RequestStatusRepository } from 'src/app/domain/request-status/request-status.repository';
 import { RequestRepository } from 'src/app/domain/request-status/request.repository';
-import { SignupRepository } from 'src/app/domain/signup/signup.repository';
 
 @Component({
   selector: 'app-application-status',
@@ -10,15 +9,48 @@ import { SignupRepository } from 'src/app/domain/signup/signup.repository';
   styleUrls: ['./application-status.component.css']
 })
 export class ApplicationStatusComponent {
+  statusValue:string="";
+  requestValue:string = "";
 
-  constructor(private router: Router,private requestRepository:RequestStatusRepository,
-    private userRepository:RequestRepository) {}
+  constructor(private router: Router,private reqStatusRepository:RequestStatusRepository,
+    private ReqRepository:RequestRepository) {}
 
   startApplication() {
-    this.userRepository.add({}).subscribe(_=>
+    this.reqStatusRepository.get().subscribe({
+    error: () =>
       {
+        this.ReqRepository.add({}).subscribe(_=>{
+        this.router.navigate(['/voter-data'])
+      });
+      },
+      next: (res) => {
+      if (res.requestStatus.code == "DRAFT")
       this.router.navigate(['/voter-data'])
+            }
     });
   }
+  ngOnInit(){
+    this.startbtn();
+  }
+  startbtn(){
+    this.reqStatusRepository.get().subscribe({
+      error: () =>
+      {
+        this.requestValue = "بدأ الطلب",
+        this.statusValue="تقديم الطلب"
+      },
+      next: (res) => {
+      if (res.requestStatus.code == "DRAFT")
+      {
+        this.requestValue = "غير مكتمل"
+        this.statusValue="استكمال الطلب";
+      }
+        else if(res.requestStatus.code == "NEW_REQUEST")
+        {
+          this.requestValue = "تم تقديم الطلب"
+        }
+            }
+  });
 
+  }
 }
